b76dcb74caf889402d06b5b757a3b00968a5a050
Belen Enemark <belenenemark@gmail.com>
2018-10-25 21:33:42 -0300

    base de datos

--- SQL/ropa.sql
+++ SQL/ropa.sql
@@ -1,69 +1,155 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.1.32-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win32
--- HeidiSQL Version:             9.5.0.5196
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-10-2018 a las 02:31:38
+-- Versión del servidor: 10.1.32-MariaDB
+-- Versión de PHP: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Dumping structure for table dbropa.categoria
-CREATE TABLE IF NOT EXISTS `categoria` (
-  `idcategoria` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Base de datos: `dbropa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `idcategoria` int(11) NOT NULL,
   `indumentaria` varchar(45) DEFAULT NULL,
-  `tipo_publico` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`idcategoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+  `tipo_publico` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table dbropa.categoria: ~8 rows (approximately)
-/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
 INSERT INTO `categoria` (`idcategoria`, `indumentaria`, `tipo_publico`) VALUES
-	(1, 'Pantalones', 1),
-	(2, 'Remera Mujer', 1),
-	(3, 'Remera Hombre', 1),
-	(4, NULL, 1),
-	(5, NULL, 1),
-	(6, NULL, 1),
-	(7, NULL, 1),
-	(8, 'buzo', 1);
-/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+(17, 'Buzos', 1),
+(18, 'Remeras', 1),
+(19, 'Jean', 1),
+(20, 'Camisas', 1);
 
--- Dumping structure for table dbropa.producto
-CREATE TABLE IF NOT EXISTS `producto` (
-  `idproducto` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `idproducto` int(11) NOT NULL,
   `nombre` varchar(45) NOT NULL,
   `precio` decimal(11,2) DEFAULT NULL,
-  `idcategoria` int(11) NOT NULL,
-  PRIMARY KEY (`idproducto`),
-  KEY `fk_producto_categoria_idx` (`idcategoria`),
-  CONSTRAINT `fk_producto_categoria` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  `idcategoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table dbropa.producto: ~2 rows (approximately)
-/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+--
+-- Volcado de datos para la tabla `producto`
+--
+
 INSERT INTO `producto` (`idproducto`, `nombre`, `precio`, `idcategoria`) VALUES
-	(1, 'PANTALON ROSA XS ', 200.00, 1),
-	(2, 'Remera Mujer L', 150.00, 2);
-/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
+(11, 'Buzo azul tam XS', '120.00', 17),
+(12, 'Buzo azul tam S', '120.00', 17),
+(13, 'Buzo azul tam L', '120.00', 17),
+(14, 'Remera rosa ', '150.00', 18),
+(15, 'Remera Gris ', '150.00', 18),
+(16, 'Remera Azul ', '155.00', 18),
+(17, 'Jean tam 40 mujer', '300.00', 19),
+(18, 'Jean tam  38 mujer', '160.00', 19),
+(19, 'Camisa Gris Hombre', '150.00', 20),
+(20, 'Camisa a cuadros ', '155.00', 20);
 
--- Dumping structure for table dbropa.usuario
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `idusuario` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idusuario` int(11) NOT NULL,
   `nombre` varchar(45) NOT NULL,
-  `clave` varchar(100) NOT NULL,
-  PRIMARY KEY (`idusuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `clave` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table dbropa.usuario: ~1 rows (approximately)
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
 INSERT INTO `usuario` (`idusuario`, `nombre`, `clave`) VALUES
-	(3, 'romeo', '$2y$10$vnUZMEAu5t53IWT/1BbleuB12WLmD0FZ8UUuL.e.i31SqYe1b9W5q');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+(3, 'romeo', '$2y$10$vnUZMEAu5t53IWT/1BbleuB12WLmD0FZ8UUuL.e.i31SqYe1b9W5q'),
+(4, 'belen', '$2y$10$OjCSB/VqXxOmC8Y0cxFXv.1SR.Q4uu6JBAikeazZw6Eky15Kob3SK'),
+(5, 'matias', '$2y$10$kRr81Mzwwzo5VYZ8wPDzue7p9UYsTh843IBEBSelroXvOa.TplYIG');
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idcategoria`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`idproducto`),
+  ADD KEY `fk_producto_categoria_idx` (`idcategoria`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idusuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `idproducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `fk_producto_categoria` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`) ON DELETE CASCADE ON UPDATE NO ACTION;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
